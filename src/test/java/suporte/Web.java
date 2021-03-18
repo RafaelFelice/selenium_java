a3c85fd62678146502dcd60c040569df0db975a1
Rafael de Felice <rafaelfelice92@email.com>
2021-03-18 16:19:14 -0300

    Eliminando lixos e colocando navegador maximizado

--- src/test/java/suporte/Web.java
+++ src/test/java/suporte/Web.java
@@ -7,12 +7,11 @@ import java.util.concurrent.TimeUnit;
 
 public class Web {
     public static WebDriver createCrhome() {
-        System.setProperty("Webdriver.chrome.driver","C:\\Users\\lucas\\Desktop\\ProjetoServeRest.exe");
         WebDriver navegador = new ChromeDriver();
 
         navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        navegador.manage().window().maximize();
 
-        // Navegando para a págnina Teste ServeRest
         navegador.get("https://serverest.netlify.app/login");
 
         return navegador;
